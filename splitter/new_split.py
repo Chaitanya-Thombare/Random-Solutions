9fa45ddfa40a3e5425b300880a6301566bc29e13
Chaitanya <indcthombare@gmail.com>
2024-01-15 13:46:39 +0530

    minor changes, and added a help file

--- splitter/new_split.py
+++ splitter/new_split.py
@@ -1,15 +1,23 @@
+# Imports
 import pandas as pd
 
+# Fetch excel file
 path = r'./splitter.xlsx'
 df = pd.read_excel(path, sheet_name='Sheet2')
 
+# Using the last column name to get all people involved
 peeps_col = df.columns[-1]
-peeps = df.columns[-1].split(',')
+peeps = peeps_col.replace(" ", "").split(',')
 
+# Create dictionary to keep track of individual
 main_chart = dict.fromkeys(peeps, 0)
 spent_chart = dict.fromkeys(peeps, 0)
 consumed_chart = dict.fromkeys(peeps, 0)
 
+# Process expenses to main, spent and consumed chart. 
+# Spent chart is individual expenditure. 
+# Consumed chart is individual consumption.
+# Main chart is net, i.e. expenditure - consumption
 for expense in df.to_dict(orient='records'):
     main_chart[expense['Sponsor']] -= expense['Cost']
     spent_chart[expense['Sponsor']] += expense['Cost']
@@ -19,15 +27,22 @@ for expense in df.to_dict(orient='records'):
         main_chart[peep] += per_head_cost
         consumed_chart[peep] += per_head_cost
 
+# Seperate creditors and debtors and cleared individual
 creds, debts, cleared = {}, {}, {}
 for k, v in main_chart.items():
     if v < 0: creds[k] = -v
     if v > 0: debts[k] = v
     if v == 0: cleared[k] = v
 
+# Sorting them in decending order of value.
 creds = dict(sorted(creds.items(), key=lambda x: x[1], reverse=True))
 debts = dict(sorted(debts.items(), key=lambda x: x[1], reverse=True))
 
+# Balancing all debtors and creditors. 
+# Logic - If Debt1 owes 5, Cred1 shall get 2 and Cred2 shall get 4.
+# Debt1 will pay Cred1 2 and Cred2 3. 
+# Cred2 shall get outstanding amount from next Debtor.
+# This shall balance because debitors shall owe exact amount as creditors have loaned
 creds_keys = list(creds.keys())
 debts_keys = list(debts.keys())
 
